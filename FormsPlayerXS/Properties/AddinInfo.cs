b9c2b0fc9d402cc528ea9f246a336eb55abbd81c
Prashant Cholachagudda <pvc@outlook.com>
2015-06-10 19:09:50 +0530

    Added the Publish Menu Item

---
+++ FormsPlayerXS/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"FormsPlayerXS", 
+	Namespace = "FormsPlayerXS",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("FormsPlayerXS")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("FormsPlayerXS")]
+[assembly:AddinAuthor ("Prashant")]
+
